c3e4a24985ae845f03948cb4de21954083679579
Aude Sedillo <aude.sedillo@gmail.com>
2024-05-16 14:37:36 +0200

    solo page css

--- src/pages/SoloPage.jsx
+++ src/pages/SoloPage.jsx
@@ -1,10 +1,17 @@
 import Header from "../components/Header";
+import "./SoloPage.css"
+import { FaCirclePlay } from "react-icons/fa6";
+
 
 function SoloPage() {
   return (
     <>
       <Header />
-      <h1>SOLO PAGE</h1>
+      <section className="display-solo-page">
+      <div className="play-solo">
+       <FaCirclePlay className="btn-solo-sound" />
+      </div>
+      </section>
     </>
   );
 }
