7eabd69d5d15023b2c0ee96bed6c2d02a2b05ae3
Aude Sedillo <aude.sedillo@gmail.com>
2024-05-16 15:49:03 +0200

    fixed css solo-page + data.js

--- src/pages/SoloPage.jsx
+++ src/pages/SoloPage.jsx
@@ -5,14 +5,15 @@ import { FaCirclePlay } from "react-icons/fa6";
 
 function SoloPage() {
   return (
-    <>
+    <div>
       <Header />
-      <section className="display-solo-page">
+    <section className="display-solo-page">
       <div className="play-solo">
        <FaCirclePlay className="btn-solo-sound" />
       </div>
-      </section>
-    </>
+      <button className="btn-laught">Ris encore</button>
+    </section>
+    </div>
   );
 }
 
