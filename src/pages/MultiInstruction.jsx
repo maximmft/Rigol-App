e22b97ffd0d1a621a2eae084be1c39cd2e1b7627
Aude Sedillo <aude.sedillo@gmail.com>
2024-05-17 12:25:27 +0200

    add modal instruction during game

---
+++ src/pages/MultiInstruction.jsx
@@ -0,0 +1,49 @@
+import "./GameInstruction.css";
+import a_vos_rire from "../assets/sounds/A-vos-rire.mp3";
+
+function MultiInstruction({ setMenuOpen }) {
+  const handleCloseInstruction = () => {
+    setMenuOpen(false);
+  };
+
+  return (
+    <div className="section-instruction-solo">
+      <button
+        type="button"
+        onClick={handleCloseInstruction}
+        className="btn-close"
+      >
+        X
+      </button>
+      <p className="paragraph-instructions">
+        L'application choisit aléatoirement une personne parmi les participants
+        pour commencer.
+        <br></br>
+        <br></br>
+        <b style={{ fontSize: "1.1rem" }}>
+          La personne désignée doit faire le rire qu’il veut
+        </b>{" "}
+        en appuyant sur le bouton de l'application.
+        <br></br>
+        <br></br>Une fois que le rire initial est déclenché,{" "}
+        <b style={{ fontSize: "1.1rem" }}>
+          les autres participants doivent imiter le rire à leur tour.
+        </b>
+        <br></br>
+        <br></br>Après que tout le groupe ait ri ensemble, l'application choisit
+        automatiquement une nouvelle personne pour déclencher son rire, et le
+        processus se répète.
+        <br></br>
+        <br></br>Les participants peuvent varier les types de rires à chaque
+        tour pour ajouter de la diversité et de l'amusement.
+        <br></br>
+        <br></br>A la fin de la séance,{" "}
+        <b style={{ fontSize: "1.1rem" }}>prenez le temps de vous remercier </b>
+        pour ce moment ensemble en vous regardant droit dans les yeux
+      </p>
+      <audio controls src={a_vos_rire}></audio>
+    </div>
+  );
+}
+
+export default MultiInstruction;
--- src/pages/SoloPage.jsx
+++ src/pages/SoloPage.jsx
@@ -4,11 +4,12 @@ import data from "../data";
 import "./SoloPage.css";
 import { FaCirclePlay } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
+import SoloInstruction from "./SoloInstruction";
 
 function SoloPage() {
   const [changeButton, setChangeButton] = useState(false);
   const [soundIndex, setSoundIndex] = useState(0);
-  const [menuOpen, setMenuOpen]=useState(false)
+  const [menuOpen, setMenuOpen] = useState(false);
   const [playSound] = useSound(data[soundIndex].sound);
 
   const navigate = useNavigate();
@@ -21,56 +22,41 @@ function SoloPage() {
   if (soundIndex >= data.length - 1) {
     navigate("/finish");
   }
-  const handleClick =()=>{
-    setMenuOpen(!menuOpen)
-  }
+  const handleClick = () => {
+    setMenuOpen(!menuOpen);
+    handleInstruction();
+  };
+
+  const handleInstruction = () => {
+    setMenuOpen(true);
+  };
 
   return (
     <>
-      <button onClick={handleClick} className="menu-instruction">Instructions</button>
-      <div className="text-instruction"> 
-      {menuOpen && (
-        <p className="paragraph-instructions">
-        Trouvez un espace calme où vous pouvez vous détendre sans être dérangé.
-        <br></br>
-        <br></br>
-        Asseyez-vous ou tenez-vous debout dans une posture confortable, le dos
-        droit et les épaules détendues.
-        <br></br>
-        <br></br><b style={{fontSize:'1.1rem'}}>Appuyez sur le bouton de lecture pour démarrer l'audio du rire. </b> 
-        Laissez-vous aller et écoutez le son du rire avec attention.
-        <br></br>
-        <br></br>Une fois l'audio du rire terminé, <b style={{fontSize:'1.1rem'}}>imitez-le du
-        mieux que vous le pouvez.</b> Laissez votre rire être authentique et
-        spontané, sans vous inquiéter de son apparence ou de sa sonorité.
-        <br></br>
-        <br></br>
-        <b style={{fontSize:'1.1rem'}}>Laissez-vous emporter par le rire contagieux.</b> Sentez la joie et la
-        légèreté envahir votre être. N'hésitez pas à rire de vous-même et à vous
-        laisser aller à l'amusement.
-        <br></br>
-        <br></br>
-        Après avoir ri pendant quelques instants, prenez une courte pause pour respirer, puis préparez-vous à répéter le
-        processus. <b style={{fontSize:'1.1rem'}}>Continuez à suivre les instructions et à rire avec abandon.</b>
-      </p>
-      )}
+      <button onClick={handleClick} className="menu-instruction">
+        Instructions
+      </button>
+      <div className="text-instruction">
+        {menuOpen && <SoloInstruction setMenuOpen={setMenuOpen} />}
       </div>
-    <div>
-      <section className="display-solo-page">
-        <div className="play-solo">
-          {changeButton === false ? (
-            <FaCirclePlay onClick={handleChange} className="btn-solo-sound" />
+      <div>
+        <section className="display-solo-page">
+          <div className="play-solo">
+            {changeButton === false ? (
+              <FaCirclePlay onClick={handleChange} className="btn-solo-sound" />
+            ) : (
+              <h2 className="btn-change-solo">A toi de rire !</h2>
+            )}
+          </div>
+          {changeButton === true ? (
+            <button onClick={handleChange} className="btn-laught">
+              Ris encore
+            </button>
           ) : (
-            <h2 className="btn-change-solo">A toi de rire !</h2>
+            <h2 className="hide-text">Prêt à rire ?</h2>
           )}
-        </div>
-        {changeButton === true ?(
-          <button onClick={handleChange} className="btn-laught">
-            Ris encore
-          </button>
-        ): <h2 className="hide-text">Prêt à rire ?</h2>}
-      </section>
-    </div>
+        </section>
+      </div>
     </>
   );
 }
