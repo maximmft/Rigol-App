65bd3ea6413c5892003fe60f361bd313c1df9cd7
Pcsrio <airyoldi@gmail.com>
2024-05-17 09:25:27 +0200

    HomeFinish

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,6 +11,8 @@ function HomePage() {
 
 	return (
 		<>
+			<Header />
+
 			<h1 className="title">Le yoga du rire c'est quoi ?</h1>
 
 			<div className="allText">
@@ -38,20 +40,10 @@ function HomePage() {
 					</button>
 				</div>
 				<div className="buuton2">
-					<button
-						className="button-MULTI"
-						onClick={() => handleClick("multiaccueil")}
-					>
-						Rire à plusieurs
-					</button>
+					<button className="button-MULTI" onClick={() => handleClick("multipage")}>Rire à plusieurs</button>
 				</div>
 				<div className="button3">
-					<button
-						className="button-home-instructions"
-						onClick={() => handleClick("instructions")}
-					>
-						Instructions
-					</button>
+					<button className="button-instructions" onClick={() => handleClick("instructions")}>Instructions</button>
 				</div>
 			</div>
 		</>
