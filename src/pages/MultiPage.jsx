6af5410c4d38a575ae31a27c512f60607d195e66
YannLahellec <lahellec.yann@gmail.com>
2024-05-17 10:04:55 +0200

    jai mal fait mon pull sorry coeur coeur sur vous

--- src/pages/MultiPage.jsx
+++ src/pages/MultiPage.jsx
@@ -1,40 +1,40 @@
-import { useState } from "react"
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import { useName } from "../contexts/name";
-import logo from "../assets/images/buddha 1.svg"
-import './MultiPage.css'
+import "./MultiPage.css";
 
 function MultiPage() {
-
   const [changeInterface, setChangeInterface] = useState(true);
 
-  const navigate = useNavigate();
-  const { names, setNames } = useName();
+  const { names } = useName();
   const [namesIndex, setNamesIndex] = useState(0);
 
-  const handleCount =() => {
-    setChangeInterface(!changeInterface)
-    if(changeInterface === true){
-    setNamesIndex(namesIndex + 1)
-    if (namesIndex >= names.length-1) {
-      setNamesIndex(0)
-    } 
+  const handleCount = () => {
+    setChangeInterface(!changeInterface);
+    if (changeInterface === true) {
+      setNamesIndex(namesIndex + 1);
+      if (namesIndex >= names.length - 1) {
+        setNamesIndex(0);
+      }
     }
-  }
-  
+  };
+
   return (
     <section className="section-multi">
-      {changeInterface === true ? <div className="play">
-        <h2 className="h2-multi" >{names[namesIndex].name}</h2>
-        <h2 className="h2-multi">Rigole un coup !</h2>
-      </div> : <div className="play">
-        <h2 className="h2-multi">A vos rires !</h2>
-      </div> }
-      <button className="button-multi" onClick={handleCount}>Suivant</button>
+      {changeInterface === true ? (
+        <div className="play">
+          <h2 className="h2-multi">{names[namesIndex].name}</h2>
+          <h2 className="h2-multi">Rigole un coup !</h2>
+        </div>
+      ) : (
+        <div className="play">
+          <h2 className="h2-multi">A vos rires !</h2>
+        </div>
+      )}
+      <button className="button-multi" onClick={handleCount}>
+        Suivant
+      </button>
     </section>
   );
 }
 
 export default MultiPage;
-
-
